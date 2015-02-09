f7decf24fcc34357f21bb6b4fd1ccaab9f575f00
Maksimanya <maxizmir@yahoo.com>
2015-02-09 10:40:50 +0000

    finish_reguistration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  get 'signup', to: 'users#new'
 
+  root 'static_pages#home'
+ 
+  resources :users
+  
   get 'contact', to: 'static_pages#contact'
 
   get 'help', to: 'static_pages#help'
