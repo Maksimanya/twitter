acf26716f3f7c2bc3bb90868a8b4845b172125d0
Maksimanya <maxizmir@yahoo.com>
2015-02-11 09:02:58 +0000

    Sign in/Sing out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save == true
-      flash[:success]="Registration has been completed!"
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
  
   resources :users
   
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+  
   get 'contact', to: 'static_pages#contact'
 
   get 'help', to: 'static_pages#help'
